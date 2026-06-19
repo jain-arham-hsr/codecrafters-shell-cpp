0d5230ee1dbdb27c94396b7c77d2d9380e5e342e
Arham Jain <jain.arham.hsr@gmail.com>
2026-06-19 12:55:33 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  // std::cout << "$ ";
+    // TODO: Uncomment the code below to pass the first stage
+    std::cout << "$ ";
 }
